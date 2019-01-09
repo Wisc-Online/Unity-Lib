87e98017ea3cdc9843b659cf21d87a007e0eaece
Brett Sheleski <brett@sheleski.com>
2019-01-09 08:55:55 -0600

    added git-update menu item

---
+++ Scripts/Editor/GitUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace FVTC.LearningInnovations.Unity.Editor
+{
+    class GitUpdate
+    {
+        const string UNITY_LIB_MODULE_URL = "https://github.com/Wisc-Online/Unity-Lib.git";
+
+        [MenuItem("Learning Innovations/Git/Update/Unity-Lib")]
+        static void Update()
+        {
+            GitHelper.UpdateSubmodule(UNITY_LIB_MODULE_URL);
+        }
+    }
+}
