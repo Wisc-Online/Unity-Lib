dd78c0d6a84051bd345963f2227dee8783fb7287
Brett Sheleski <brett@sheleski.com>
2019-02-15 10:53:33 -0600

    added building to Android platform from menu

--- Scripts/Editor/BuildMenu.cs
+++ Scripts/Editor/BuildMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -16,45 +17,95 @@ namespace FVTC.LearningInnovations.Unity.Editor
         [MenuItem("Learning Innovations/Build/Standalone/Windows (64-bit)")]
         static void BuildStandaloneWindows64()
         {
-            Build(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Standalone,
+                BuildTarget = BuildTarget.StandaloneWindows64
+            });
         }
 
         [MenuItem("Learning Innovations/Build/Standalone/Windows (32-bit)")]
         static void BuildStandaloneWindows32()
         {
-            Build(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Standalone,
+                BuildTarget = BuildTarget.StandaloneWindows
+            });
         }
-        
+
         [MenuItem("Learning Innovations/Build/Standalone/Mac")]
         static void BuildStandaloneMac()
         {
-            Build(BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Standalone,
+                BuildTarget = BuildTarget.StandaloneOSX
+            });
         }
 
         [MenuItem("Learning Innovations/Build/Standalone/Linux")]
         static void BuildStandaloneLinux()
         {
-            Build(BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Standalone,
+                BuildTarget = BuildTarget.StandaloneLinux
+            });
         }
 
-        [MenuItem("Learning Innovations/Build/Android/Android")]
+        [MenuItem("Learning Innovations/Build/Android/Android (No VR)")]
         static void BuildAndroid()
         {
-            Build(BuildTargetGroup.Android, BuildTarget.Android);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Android,
+                BuildTarget = BuildTarget.Android
+            });
+        }
+
+
+        [MenuItem("Learning Innovations/Build/Android/Google Cardboard")]
+        static void BuildAndroidGoogleCardboard()
+        {
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Android,
+                BuildTarget = BuildTarget.Android,
+                IsVirtualRealitySupported = true,
+                VirtualRealitySDKs = new string[] { "cardboard" },
+                NameSuffix = "Cardboard"
+            });
         }
 
 
         [MenuItem("Learning Innovations/Build/Android/Oculus Go")]
         static void BuildAndroidOculusGo()
         {
-            Build(BuildTargetGroup.Android, BuildTarget.Android);
+            Build(new BuildSettings
+            {
+                BuildTargetGroup = BuildTargetGroup.Android,
+                BuildTarget = BuildTarget.Android,
+                IsVirtualRealitySupported = true,
+                VirtualRealitySDKs = new string[] { "Oculus" },
+                NameSuffix = "Oculus"
+            });
         }
 
-        private static void Build(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget)
+        private static BuildSettings GetCurrentBuildSettings()
         {
-            BuildTargetGroup oldBuildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-            BuildTarget oldBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+            return new BuildSettings
+            {
+                BuildTarget = EditorUserBuildSettings.activeBuildTarget,
+                BuildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
+                IsVirtualRealitySupported = PlayerSettings.virtualRealitySupported,
+                VirtualRealitySDKs = PlayerSettings.GetVirtualRealitySDKs(EditorUserBuildSettings.selectedBuildTargetGroup),
+            };
+        }
 
+        private static void Build(BuildSettings buildSettings)
+        {
+            var oldBuildSettings = GetCurrentBuildSettings();
 
             DirectoryInfo buildOutputDirectory;
             EditorBuildSettingsScene[] scenes;
@@ -62,14 +113,17 @@ namespace FVTC.LearningInnovations.Unity.Editor
 
             EditorUtility.DisplayProgressBar("Build", "Starting the build process.", 0f);
 
-            if (oldBuildTarget != buildTarget || oldBuildTargetGroup != buildTargetGroup)
+            if (oldBuildSettings.BuildTarget != buildSettings.BuildTarget || oldBuildSettings.BuildTargetGroup != buildSettings.BuildTargetGroup)
             {
-                EditorUtility.DisplayProgressBar("Build", "Switching current build target to " + buildTargetGroup.ToString() + " - " + buildTarget.ToString(), 0f);
-                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, buildTarget);
+                EditorUtility.DisplayProgressBar("Build", "Switching current build target to " + buildSettings.BuildTargetGroup.ToString() + " - " + buildSettings.BuildTarget.ToString(), 0f);
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, buildSettings.BuildTarget);
 
                 // SwitchActiveBuildTarget clears the progress bar, so show it again
                 EditorUtility.DisplayProgressBar("Build", "Starting the build process.", 0f);
             }
+
+            PlayerSettings.virtualRealitySupported = buildSettings.IsVirtualRealitySupported;
+            PlayerSettings.SetVirtualRealitySDKs(buildSettings.BuildTargetGroup, buildSettings.VirtualRealitySDKs);
 
             if (EditorUserBuildSettings.development)
                 buildOptions |= BuildOptions.Development;
@@ -88,7 +142,7 @@ namespace FVTC.LearningInnovations.Unity.Editor
 
             var buildsDir = new DirectoryInfo(Path.Combine(projectDir.FullName, BUILDS_DIRECTORY));
 
-            buildOutputDirectory = new DirectoryInfo(Path.Combine(buildsDir.FullName, buildTarget.ToString()));
+            buildOutputDirectory = new DirectoryInfo(Path.Combine(buildsDir.FullName, buildSettings.BuildTarget.ToString()));
 
             if (!buildsDir.Exists)
                 buildsDir.Create();
@@ -96,7 +150,11 @@ namespace FVTC.LearningInnovations.Unity.Editor
             if (!buildOutputDirectory.Exists)
                 buildOutputDirectory.Create();
 
-            FileInfo buildOutputExe = new FileInfo(Path.Combine(buildOutputDirectory.FullName, string.Format("{0}.exe", PlayerSettings.productName)));
+            FileInfo buildOutputExe = new FileInfo(Path.Combine(buildOutputDirectory.FullName, string.Format("{0}{2}.{1}", 
+                PlayerSettings.productName, 
+                buildSettings.GetBuildFileExtension(),
+                string.IsNullOrEmpty(buildSettings.NameSuffix) ? "" : " (" + buildSettings.NameSuffix + ")"
+                )));
 
             BuildReport buildReport = null;
 
@@ -105,18 +163,21 @@ namespace FVTC.LearningInnovations.Unity.Editor
                 buildReport = BuildPipeline.BuildPlayer(
                         scenes,
                         buildOutputExe.FullName,
-                        buildTarget,
+                        buildSettings.BuildTarget,
                         buildOptions);
             }
             finally
             {
-                if (oldBuildTarget != buildTarget || oldBuildTargetGroup != buildTargetGroup)
+                if (oldBuildSettings.BuildTarget != buildSettings.BuildTarget || oldBuildSettings.BuildTargetGroup != buildSettings.BuildTargetGroup)
                 {
-                    EditorUtility.DisplayProgressBar("Build", "Switching current build target back to " + oldBuildTargetGroup.ToString() + " - " + oldBuildTarget.ToString(), 0f);
-                    EditorUserBuildSettings.SwitchActiveBuildTarget(oldBuildTargetGroup, oldBuildTarget);
+                    EditorUtility.DisplayProgressBar("Build", "Switching current build target back to " + buildSettings.BuildTargetGroup.ToString() + " - " + buildSettings.BuildTarget.ToString(), 0f);
+                    EditorUserBuildSettings.SwitchActiveBuildTarget(buildSettings.BuildTargetGroup, oldBuildSettings.BuildTarget);
                 }
 
                 EditorUtility.ClearProgressBar();
+
+                PlayerSettings.virtualRealitySupported = oldBuildSettings.IsVirtualRealitySupported;
+                PlayerSettings.SetVirtualRealitySDKs(oldBuildSettings.BuildTargetGroup, oldBuildSettings.VirtualRealitySDKs);
 
                 switch (buildReport.summary.result)
                 {
@@ -137,8 +198,27 @@ namespace FVTC.LearningInnovations.Unity.Editor
             }
         }
 
-       
 
+        public class BuildSettings
+        {
+            public BuildTargetGroup BuildTargetGroup { get; set; }
+            public BuildTarget BuildTarget { get; set; }
+            public bool IsVirtualRealitySupported { get; set; }
+            public string[] VirtualRealitySDKs { get; set; }
+            public string NameSuffix { get; internal set; }
+
+            public string GetBuildFileExtension()
+            {
+                switch (BuildTargetGroup)
+                {
+                    case BuildTargetGroup.Android:
+                        return "apk";
+                }
+
+                // if all else fails, assume .exe
+                return "exe";
+            }
+        }
 
         static void DeleteDirectory(string target_dir)
         {
