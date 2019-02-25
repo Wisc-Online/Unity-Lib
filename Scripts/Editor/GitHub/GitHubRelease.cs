aca65670e978e8c23427d1377c8c8dfd04166cbc
Brett Sheleski <brett@sheleski.com>
2019-02-25 10:54:46 -0600

    added GitHub classes for downloading releases

---
+++ Scripts/Editor/GitHub/GitHubRelease.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace FVTC.LearningInnovations.Unity.Editor.GitHub
+{
+    [Serializable]
+    public class GitHubRelease
+    {
+        public string url;
+        public string assets_url;
+        public string upload_url;
+        public string html_url;
+        public int id;
+        public string node_id;
+        public string tag_name;
+        public string target_commitish;
+        public string name;
+        public bool draft;
+        public GitHubReleaseAuthor author;
+        public bool prerelease;
+        public DateTime created_at;
+        public DateTime published_at;
+        public GitHubReleaseAsset[] assets;
+        public string tarball_url;
+        public string zipball_url;
+        public string body;
+
+        public static GitHubRelease Download(string url)
+        {
+            return Download(url, null);
+        }
+
+        public static GitHubRelease Download(string url, Action<float> downloadProgressCallback)
+        {
+            WWW www = new WWW(url);
+
+            while (!www.isDone)
+            {
+                if (downloadProgressCallback != null)
+                    downloadProgressCallback(www.progress);
+            }
+
+            if (www.error == null)
+            {
+                var release = JsonUtility.FromJson<GitHubRelease>(www.text);
+
+                return release;
+            }
+            else
+            {
+                Debug.LogError(www.error);
+                return null;
+            }
+        }
+    }
+}
---
+++ Scripts/Editor/GitHub/GitHubReleaseAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FVTC.LearningInnovations.Unity.Editor.GitHub
+{
+    [Serializable]
+    public class GitHubReleaseAsset
+    {
+        public string url;
+        public int id;
+        public string node_id;
+        public string name;
+        public object label;
+        public GitHubReleaseUploader uploader;
+        public string content_type;
+        public string state;
+        public int size;
+        public int download_count;
+        public DateTime created_at;
+        public DateTime updated_at;
+        public string browser_download_url;
+    }
+}
